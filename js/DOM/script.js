9b0e7a2fa65a1a25f65de99871e9043aaf2cd870
rish0000-dot <rs9076738@gmail.com>
2025-11-10 15:00:11 +0530

    chess

---
+++ js/DOM/script.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById("main-heading");
+console.log(heading);
+heading.style.color = "blue";
+heading.textContent = "Hello from script.js file";
+heading.style.backgroundColor = "aqua";
+heading.style.fontSize = "80px";
