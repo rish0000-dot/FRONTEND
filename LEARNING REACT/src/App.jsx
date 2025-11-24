ef9beadd0e7e164a3680f97f69e7b2429be936fc
rish0000-dot <rs9076738@gmail.com>
2025-11-24 08:18:59 +0530

    jsx

---
+++ LEARNING REACT/src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <Card title="Card Title" description="This is the content of the card." />
+      <Card title="Another Card" description="This is another card description." />
+    </div>
+  );
+}
+export default App;
---
+++ LEARNING REACT/src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+function Card(props) {
+    const { title, description } = props;
+    return <>
+        <h1>{title}</h1>
+        <p>{description}</p>
+    </>
+  
+}    
+export default Card;
